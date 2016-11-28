0df17d807e9926fb725a38dfafa94b727dec14b2
ianianda <ywang402@my.bcit.ca>
2016-11-27 21:15:30 -0800

    Update index.php

--- index.php
+++ index.php
@@ -7,32 +7,49 @@
   <meta charset="utf-8" />
 </head>
 <body>
-    <div class="container"><div class="row">
-    <div class="col-sm-10 col-sm-offset-1"><h1>Please login to enjoy our math game.</h1></div>
-    <div class="col-sm-1"></div>
-</div>
-<form action="authenticate.php" method="post" role="form" class="form-horizontal">
-    <div class="form-group">
-        <div class="col-sm-4 text-right">Email:</div>
-        <div class="col-sm-3">
-            <input type="text" class="form-control" id="email" name="email" placeholder="Email" size="6">
-        </div>
-        <div class="col-sm-5"></div>
+    <div class="container">
+<form action="index.php" method="post" role="form" class="form-horizontal">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4"><h1>Math Game</h1></div>
+        <div class="col-sm-4"><a href="logout.php" class="btn btn-default btn-sm">Logout</a></div>
     </div>
+    <div class="row">
+        <label class="col-sm-2 col-sm-offset-3">7</label>
+        <label class="col-sm-2">+</label>
+        <label class="col-sm-2">15</label>
+        <div class="col-sm-3"></div>
+    </div>
+
+    <input type="hidden" name="first_number" value="7" />
+    <input type="hidden" name="operation" value="+" />
+    <input type="hidden" name="second_number" value="15" />
+    <input type="hidden" name="total" value="0" />
+    <input type="hidden" name="score" value="0" />
+
     <div class="form-group">
-        <div class="col-sm-4 text-right">Password:</div>
-        <div class="col-sm-3">
-            <input type="text" class="form-control" id="password" name="password" placeholder="Password" size="6">
+        <div class="col-sm-3 col-sm-offset-4">
+            <input type="text" class="form-control" id="answer" name="answer" placeholder="Enter answer" size="6">
         </div>
         <div class="col-sm-5"></div>
     </div>
     <div class="row">
         <div class="col-sm-3 col-sm-offset-4">
-            <button type="submit" class="btn btn-primary">Login</button>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button type="submit" class="btn btn-primary btn-sm">
+            Submit</button>
         </div>
+        <div class="col-sm-3"></div>
     </div>
 </form>
+<hr />
 <div class="row">
+    <div class="col-sm-4 col-sm-offset-4">
+            </div>
+    <div class="col-sm-4"></div>
+</div>
+<div class="row">
+    <div class="col-sm-4 col-sm-offset-4">Score: 0 / 0</div>
+    <div class="col-sm-4"></div>
 </div>
     </div>
 </body>
