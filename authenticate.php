9646f3a04cbef3c676310d64caaa8d5bf1e769af
ianianda <ywang402@my.bcit.ca>
2016-11-28 18:48:43 -0800

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -59,12 +59,12 @@
 		
 		<?php 
 		$sub_answer =0;
-		if ($_SESSION['textboxError'] = true;) {
+		if ($_SESSION['textboxError'] == true;) {
                 	echo "<font color='red'>You must enter a number for your answer.</font>";
 		} 	
-  	        else if ($_POST['textbox'] != $sub_answer){
-			echo "your answer is incorrect";
-		}
+  	        //else if ($_POST['textbox'] != $sub_answer){
+		//	echo "your answer is incorrect";
+		//}
 		?>
         </div>
         <div class="col-sm-5">
