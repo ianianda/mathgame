75ce99312cbb6f77ff0428cea3954547203188dc
ianianda <ywang402@my.bcit.ca>
2016-11-28 21:16:50 -0800

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -63,7 +63,7 @@
 			} else if ($_SESSION['key'] == $answer){
 			    echo "<font color='green'>Correct</font>";
 			} else if ($_SESSION['key'] != $answer){
-			    echo '<span style="color: red; font-weight: bold;">INCORRECT, ' . $_SESSION['first_number'] . ' ' . $_SESSION['operator'] . ' ' . $_SESSION['second_number'] . ' is ' . $_SESSION['key'] . '.</span>';
+			    echo '<span style="color: red; font-weight: bold;">INCORRECT, ' . $_SESSION['first_number'] . ' ' . $_SESSION['operator'] . ' ' . $_SESSION['second_number'] . ' is ' . $key . '.</span>';
 			}
 			$_SESSION['key'] = $key;
 		    $_SESSION['first_number'] = $first_number;
