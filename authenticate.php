525e7f9d45206fecd7c809489fc4dab54ab55c83
ianianda <ywang402@my.bcit.ca>
2016-11-28 17:42:09 -0800

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -57,7 +57,7 @@
 		<?php 
 		if (isset($_SESSION['textboxError']) || $_SESSION['textboxError'] == true) {
                 echo "<font color='red'>You must enter a number for your answer.</font>";} 	
-  	        else if ($_SESSION["textbox"] == $sub_answer){
+  	        else if ($_POST["textbox"] == $sub_answer){
 		echo your answer is right;}
 		?>
         </div>
