98eff13141349e1259a63fd2afef8deeb5f2f6a7
ianianda <ywang402@my.bcit.ca>
2016-11-28 23:49:46 -0800

    Create authenticate.php

---
+++ authenticate.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if(isset($_SESSION['login']) && $_SESSION['login'] == 'yes') {
+	header('Location: index.php');
+}
+$error = false;
+if(!isset($_POST['email']) || $_POST['email'] !== "a@a.a") {
+  $error = true;
+}
+if(!isset($_POST['password']) || $_POST['password'] !== "aaa") {
+  $error = true;
+}
+if($error) {
+  header("Location: login.php?msg=Invalid%20login%20credentials.");
+} else {
+  $_SESSION['login'] = 'yes';
+  header("Location: index.php");
+  die();
+}
+?>
