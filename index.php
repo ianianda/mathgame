ee4d4ed7cdbe578c66bc5cca867209c5541babc9
ianianda <ywang402@my.bcit.ca>
2016-11-29 00:04:57 -0800

    Update index.php

--- index.php
+++ index.php
@@ -101,7 +101,10 @@
     <div class="col-sm-4"></div>
 </div>
 <div class="row">
-    <div class="col-sm-4 col-sm-offset-4"><?php echo 'Score: ' . $score . ' ' . $_POST["score"] . ' ' . $_SESSION['score'] . ' / ' . $total; ?></div>
+    <div class="col-sm-4 col-sm-offset-4"><?php echo 'Score: ' . $score .  ' / ' . $total; ?></div>
+	<?php
+	' ' . $_POST["score"] . ' ' . $_SESSION['score'] .
+	?>
     <div class="col-sm-4"></div>
 </div>
     </div>
