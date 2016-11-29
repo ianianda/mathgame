12e95fe9abf86f84726413de4d2681439cf60ca8
ianianda <ywang402@my.bcit.ca>
2016-11-29 15:24:43 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,22 +2,20 @@
 <html lang="en">
     <?php
         session_start();
-	if(isset($_SESSION['login']) && $_SESSION['login'] == 'yes') {
-	header('Location: index.php');
-}
-$error = false;
-if(!isset($_POST['email']) || $_POST['email'] !== "a@a.a") {
-  $error = true;
-}
-if(!isset($_POST['password']) || $_POST['password'] !== "aaa") {
-  $error = true;
-}
-if($error) {
-  header("Location: login.php?msg=Invalid%20login%20credentials.");
-} else {
-  $_SESSION['login'] = 'yes';
-  header("Location: index.php");
-  die();
+	$error = false;
+	if(!isset($_POST['email']) || $_POST['email'] !== "a@a.a") {
+  	    $error = true;
+	}
+	else if(!isset($_POST['password']) || $_POST['password'] !== "aaa") {
+  	    $error = true;
+	}
+
+	if($error) {
+  	    header("Location: login.php?msg=Invalid%20login%20credentials.");
+	} else {
+            $_SESSION['login'] = 'yes';
+            header("Location: index.php");
+            die();
 }
         if (!isset($_SESSION['score'])){
 	    $score = 0;
