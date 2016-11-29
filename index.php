579d8852a2a45d20582c19c318acdfd386d29cf7
ianianda <ywang402@my.bcit.ca>
2016-11-29 15:28:29 -0800

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,9 @@
 <html lang="en">
     <?php
         session_start();
-	$error = false;
-	if(!isset($_POST['email']) || $_POST['email'] !== "a@a.a") {
-  	    $error = true;
-	}
-	else if(!isset($_POST['password']) || $_POST['password'] !== "aaa") {
-  	    $error = true;
-	}
 
-	if($error) {
-  	    header("Location: login.php?msg=Invalid%20login%20credentials.");
-	} else {
-            $_SESSION['login'] = 'yes';
-            header("Location: index.php");
-            die();
+	if(empty($_SESSION['login'])) {
+	header('Location: login.php');
 }
 	
 	
