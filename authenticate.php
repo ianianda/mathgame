8c99c8cfe75e2f48751ebddd5183d7a1b917743f
ianianda <ywang402@my.bcit.ca>
2016-11-29 17:44:05 -0800

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,20 +1,19 @@
 <?php
     session_start();
-    $error = false;
 
     if(isset($_SESSION['login']) && $_SESSION['login'] == 'yes') {
 	header('Location: index.php');
         }
 
-    if(!isset($_POST['email']) || $_POST['email'] !== "a@a.a") {
+    else if(!isset($_POST['email']) || $_POST['email'] !== "a@a.a") {
         header("Location: login.php?msg=Invalid%20login%20credentials.");
         }
 
-    if(!isset($_POST['password']) || $_POST['password'] !== "aaa") {
+    else if(!isset($_POST['password']) || $_POST['password'] !== "aaa") {
         header("Location: login.php?msg=Invalid%20login%20credentials.");
         }
 
-     else if {
+     else {
         $_SESSION['login'] = 'yes';
         header("Location: index.php");
         die();
