eff09227f132d12a67905e89d4b916763e679edb
ianianda <ywang402@my.bcit.ca>
2016-11-29 17:31:44 -0800

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,20 +1,24 @@
 <?php
-session_start();
-if(isset($_SESSION['login']) && $_SESSION['login'] == 'yes') {
+    session_start();
+    $error = false;
+
+    if(isset($_SESSION['login']) && $_SESSION['login'] == 'yes') {
 	header('Location: index.php');
-}
-$error = false;
-if(!isset($_POST['email']) || $_POST['email'] !== "a@a.a") {
-  $error = true;
-}
-if(!isset($_POST['password']) || $_POST['password'] !== "aaa") {
-  $error = true;
-}
-if($error) {
-  header("Location: login.php?msg=Invalid%20login%20credentials.");
-} else {
-  $_SESSION['login'] = 'yes';
-  header("Location: index.php");
-  die();
-}
+        }
+
+    if(!isset($_POST['email']) || $_POST['email'] !== "a@a.a") {
+        $error = true;
+        }
+
+    if(!isset($_POST['password']) || $_POST['password'] !== "aaa") {
+        $error = true;
+        }
+
+    if($error) {
+        header("Location: login.php?msg=Invalid%20login%20credentials.");
+    } else {
+        $_SESSION['login'] = 'yes';
+        header("Location: index.php");
+        die();
+    }
 ?>
